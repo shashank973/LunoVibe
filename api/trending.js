554918745d62ac984bf900a58b9092de04680419
shashank973 <shashank973@users.noreply.github.com>
2026-06-09 18:41:21 +0530

    chore: remove duplicate root api/ functions (use client/api for Vercel)

--- api/trending.js
+++
@@ -1,6 +0,0 @@
-const { CURATED_TRACKS } = require('./_lib');
-
-module.exports = async (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).json(CURATED_TRACKS);
-};
